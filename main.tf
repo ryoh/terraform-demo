acc37bb0b880157a0b90c5d5a2609f958d99cfbb
Ryoh Kawai <kawairyoh@gmail.com>
2023-02-14 00:34:32 +0900

    Add comment

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 terraform {
   required_providers {
+    # AWS provider
+    # https://registry.terraform.io/providers/hashicorp/aws/latest
     aws = {
       source = "hashicorp/aws"
       version = "4.54.0"
